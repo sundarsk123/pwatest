1931f3ae98f7db3674a8e5cfc8ec4b09f9908467
sundardezvolta <sundar@dezvolta.com>
2025-10-30 09:39:30 +0530

    pwa install

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
+// next.config.js
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // disable in dev
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'dezvolta.in',
-            },
-        ]
-    }
-};
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dezvolta.in',
+      },
+    ],
+  },
+});
 
 export default nextConfig;
